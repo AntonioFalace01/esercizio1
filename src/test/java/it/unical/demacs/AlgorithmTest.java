68a216799a2abdd5f651cad359083adf3fdde39d
AntonioFalace01 <falaceantonio290@gmail.com>
2025-12-05 10:11:10 +0100

    creazione metodi per testrare se lastrimga inserita nulla scatena l'eccezione, e scritturw di setup per creare un oggetto Algorithms ogni volta che viene eseguiuta un metodo di test

--- src/test/java/it/unical/demacs/AlgorithmTest.java
+++ src/test/java/it/unical/demacs/AlgorithmTest.java
@@ -1,4 +1,23 @@
 package it.unical.demacs;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class AlgorithmTest {
+
+    private Algorithms algo;
+
+    @BeforeEach
+    public void setUp() {
+         algo = new Algorithms();
+    }
+
+    @Test
+    public void insertNullString(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            algo.isPalindrome(null);
+        });
+    }
 }
